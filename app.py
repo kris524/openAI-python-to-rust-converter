8c8ea1e22e8dcb7757e059a249b8c13aa6eaa6a3
Kris <kristian_dilov@abv.bg>
2023-01-14 15:44:46 +0000

    first commit

--- app.py
+++ app.py
@@ -10,11 +10,17 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 @app.route("/", methods=("GET", "POST"))
 def index():
     if request.method == "POST":
-        animal = request.form["animal"]
+        code = request.form["code"]
+
         response = openai.Completion.create(
-            model="text-davinci-003",
-            prompt=generate_prompt(animal),
-            temperature=0.6,
+            model="code-davinci-002",
+            prompt=generate_prompt(code),
+            temperature=0,
+            max_tokens=64,
+            top_p=0,
+            frequency_penalty=0,
+            presence_penalty=0,
+            stop=['"""'],
         )
         return redirect(url_for("index", result=response.choices[0].text))
 
@@ -22,14 +28,44 @@ def index():
     return render_template("index.html", result=result)
 
 
-def generate_prompt(animal):
-    return """Suggest three names for an animal that is a superhero.
+def generate_prompt(code):
+    return """
+#Convert this from Python to Rust
 
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: {}
-Names:""".format(
-        animal.capitalize()
+
+Python: a= 10
+
+#End
+
+#Rust version
+
+Rust: let a = 10;
+
+#End
+
+#Python version
+Python: {}
+# End
+
+#Rust version
+Rust:
+
+
+""".format(
+        code
     )
+
+
+# Python:
+# a = 1
+# b = 2
+# def sum(i,j): return i+j
+# res = sum(a,b)  
+# return res
+
+# Rust:
+# let a = 1;
+# let b = 2;
+# fn sum(i: i32, j: i32) -> i32 {i + j}
+# let res = sum(a, b);
+# return res;
